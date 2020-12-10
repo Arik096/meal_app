af8ba04a65da13d70a81cd749839ecc3c486c4e4
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-12-11 00:58:09 +0600

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -22,6 +24,35 @@ class AdminController extends Controller
 
         $today = Carbon::now();
         return view('admin.dashboard', compact('today','users'));
+    }
+
+    public function store(Request $request)
+    {
+        return $request;
+        /*
+        $this->validate($request,[
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+        ]);
+
+        if(!empty($request->password)){
+            $password = trim($request->password);
+        }else{
+            $password = 'password';
+        }
+        $user =new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($password);
+
+
+
+        $user->save();
+        $user->syncRoles($request->role);
+
+        return redirect()->back()->with('message', 'Added User Data!');
+        */
     }
 
 }
