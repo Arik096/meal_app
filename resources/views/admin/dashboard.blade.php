8beb9a6edbd23361f6050e318e8bc11dd258fb2f
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-12-11 01:06:33 +0600

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -53,26 +53,18 @@
                          </div>
                          <div class="modal-body">
 
-                         <form method="POST" action="{{route('userStore')}}"    enctype="multipart/form-data">@csrf
+                         <form method="POST" action="{{route('userStore')}}" enctype="multipart/form-data">@csrf
                             <div class="form-row">
 
 
                                 <label>Name</label>
                                 <input type="text" class="form-control" id="name" placeholder="Enter Name">
 
-
-
-
                                 <label>Email</label>
                                 <input type="email" class="form-control" id="email" placeholder="Enter Email">
 
-
-
-
                                 <label>Password</label>
                                 <input type="password" class="form-control" id="password" placeholder="Enter Password">
-
-
 
                                 <label>Role</label>
                                 <select id="role" class="form-control">
