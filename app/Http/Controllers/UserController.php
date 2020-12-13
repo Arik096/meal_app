324c7b563eef7fbe3b8199a54610f2d2adaa93cf
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-12-13 22:25:33 +0600

    Update UserController.php
    dynamic entry date from admin is done but the data is showing in json fromat. it needs to be make right

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class UserController extends Controller
 {
@@ -16,6 +18,10 @@ class UserController extends Controller
 
     public function dashboard()
     {
-        return view('user.dashboard');
+        $entry_time_max_id = DB::table('entry_times')->max('id');
+        $entry_time = DB::table('entry_times')->select('entry_last_time')->where('id', $entry_time_max_id)->get();
+
+
+        return view('user.dashboard', compact('entry_time'));
     }
 }
